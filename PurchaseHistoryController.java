06e85095355d147cad698b57c740be46ea39b3bf
ryutaromitsuhata19961126 <ryutaromitsuhata19961126@gmail.com>
2020-04-30 13:40:13 +0900

    商品購入履歴機能ファイルのコミット

---
+++ PurchaseHistoryController.java
@@ -0,0 +1,45 @@
+package jp.co.internous.peace.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jp.co.internous.peace.model.domain.dto.PurchaseHistoryDto;
+import jp.co.internous.peace.model.mapper.TblPurchaseHistoryMapper;
+import jp.co.internous.peace.model.session.LoginSession;
+
+@Controller
+@RequestMapping("/peace/history")
+public class PurchaseHistoryController {
+	
+	@Autowired
+	private LoginSession loginSession;
+
+	@Autowired
+	TblPurchaseHistoryMapper purchaseHistoryMapper;
+	
+	
+	@RequestMapping("/")
+	public String index(Model m) {
+
+		int userId = loginSession.getUserId();
+		List<PurchaseHistoryDto> historyList = purchaseHistoryMapper.findByUserId(userId);
+		
+		m.addAttribute("historyList", historyList);
+		m.addAttribute("loginSession", loginSession);
+		return "purchase_history";
+	}
+	
+	@RequestMapping("/delete")
+	@ResponseBody
+	public boolean delete() {
+		int userId = loginSession.getUserId();
+		int result = purchaseHistoryMapper.logicalDeleteByUserId(userId);
+		
+		return result > 0;
+	}
+}
